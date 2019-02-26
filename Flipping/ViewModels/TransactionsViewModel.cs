6d7b6e1f52955addb5cdb78f6745c294c091b7c1
Unknown <matthew.skillington@ideagenplc.com>
2019-02-26 19:46:34 +0000

    Try saving to application.current.properties

--- Flipping/ViewModels/TransactionsViewModel.cs
+++ Flipping/ViewModels/TransactionsViewModel.cs
@@ -3,17 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Flipping.ViewModels
 {
     class TransactionsViewModel : BindableObject
     {
+        public TransactionsViewModel()
+        {
+        }
 
         public ObservableCollection<Transaction> transactions = new ObservableCollection<Transaction>
         {
             new Transaction("Combat Potion(2)", 1000, 390, 415),
             new Transaction("Magic Potion(4)", 5000, 5, 25)
         };
+
+        
+
+        public ICommand AddCommand => new Command(SaveToDevice);
+
+        public void SaveToDevice()
+        {
+            transactions.Add(new Transaction("New item", 100, 10, 20));
+            var x = 0;
+            foreach(Transaction transaction in transactions)
+            {
+                Application.Current.Properties[x.ToString()] = transactions[x].Name;
+                x++;
+            }
+        }
     }
 }
